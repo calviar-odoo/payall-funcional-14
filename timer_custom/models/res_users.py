283acb49f2133db362012cfda99b24d312bdd5bd
ander94ds <ander94ds@gmail.com>
2022-09-13 14:44:11 -0400

    [DEL] deleted old module

--- timer_custom/models/res_users.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class UsersCodeInherit(models.Model):
-    _inherit = 'res.users'
-
-    helpdesk_team_id = fields.Many2many('helpdesk.team', string='Helpdesk Team', index=True)
