30b0e9a2a617d1a2c689aabd3684753fcb386170
ander94ds <ander94ds@gmail.com>
2022-09-20 15:25:43 -0400

    [RM] module

--- custom_timer/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class PartnerCodeInherit(models.Model):
-    _inherit = 'res.partner'
-
-    rif_aliado = fields.Char(string = 'RIF')
